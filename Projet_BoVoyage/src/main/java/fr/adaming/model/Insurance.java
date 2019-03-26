9ec3b4049bfe8ed358bff3907e068f7661f2d914
IN-BR-002 <IN-BR-002@IN-BR-002>
2019-03-26 20:44:18 +0100

    Ele: Commit

---
+++ Projet_BoVoyage/src/main/java/fr/adaming/model/Insurance.java
@@ -0,0 +1,16 @@
+package fr.adaming.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "insurances")
+@SuppressWarnings("serial")
+public class Insurance  implements Serializable{
+	
+	
+	
+
+}
