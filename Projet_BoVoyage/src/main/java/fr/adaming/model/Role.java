9ca3b6007e01e880f7ddb255113a2da66b64dd47
IN-BR-009 <IN-BR-009@IN-BR-009>
2019-03-26 17:52:44 +0100

    Souk : Role qui marche pour de vrai ahah

---
+++ Projet_BoVoyage/src/main/java/fr/adaming/model/Role.java
@@ -0,0 +1,74 @@
+package fr.adaming.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name="roles")
+public class Role implements Serializable {
+
+	// Attributs
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id_r")
+	private int idRole;
+	private String roleName;
+	
+	// Transformation de l'association UML en Java
+	@ManyToOne
+	@JoinColumn(name="ad_id",referencedColumnName="id_ad")
+	private Admin admin;
+
+	// Constructeur
+	public Role() {
+		super();
+	}
+
+	public Role(int idRole, String roleName, Admin admin) {
+		super();
+		this.idRole = idRole;
+		this.roleName = roleName;
+		this.admin = admin;
+	}
+
+	public Role(String roleName, Admin admin) {
+		super();
+		this.roleName = roleName;
+		this.admin = admin;
+	}
+	
+	// Getters & Setters
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public int getIdRole() {
+		return idRole;
+	}
+
+	public void setIdRole(int idRole) {
+		this.idRole = idRole;
+	}
+
+	public Admin getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(Admin admin) {
+		this.admin = admin;
+	}
+
+}
