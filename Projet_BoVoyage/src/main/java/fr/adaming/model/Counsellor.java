625ee045e92e39e526fdcef3ba6fcafed2e64dbd
IN-BR-002 <IN-BR-002@IN-BR-002>
2019-03-27 14:50:56 +0100

    Ele: Commit

--- Projet_BoVoyage/src/main/java/fr/adaming/model/Counsellor.java
+++ Projet_BoVoyage/src/main/java/fr/adaming/model/Counsellor.java
@@ -30,7 +30,76 @@ public class Counsellor implements Serializable {
 	
 	private boolean active;
 
-	@OneToMany(mappedBy="admin")
+	@OneToMany(mappedBy="counsellor")
 	private List<Role> roles;
 
+	
+	// Constructors
+	public Counsellor() {
+		super();
+	}
+
+	public Counsellor(String mailCo, String mdpCo, boolean active, List<Role> roles) {
+		super();
+		this.mailCo = mailCo;
+		this.mdpCo = mdpCo;
+		this.active = active;
+		this.roles = roles;
+	}
+
+	public Counsellor(int idCo, String mailCo, String mdpCo, boolean active, List<Role> roles) {
+		super();
+		this.idCo = idCo;
+		this.mailCo = mailCo;
+		this.mdpCo = mdpCo;
+		this.active = active;
+		this.roles = roles;
+	}
+
+	// Getters & Setters
+	public int getIdCo() {
+		return idCo;
+	}
+
+	public void setIdCo(int idCo) {
+		this.idCo = idCo;
+	}
+
+	public String getMailCo() {
+		return mailCo;
+	}
+
+	public void setMailCo(String mailCo) {
+		this.mailCo = mailCo;
+	}
+
+	public String getMdpCo() {
+		return mdpCo;
+	}
+
+	public void setMdpCo(String mdpCo) {
+		this.mdpCo = mdpCo;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+	
+	
+	
+	
+	
+	
 }
