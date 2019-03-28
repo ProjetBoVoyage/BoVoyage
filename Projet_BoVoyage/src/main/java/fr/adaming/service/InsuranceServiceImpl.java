1a35ad47044710627e49f17c4b51dc39ed081030
IN-BR-009 <IN-BR-009@IN-BR-009>
2019-03-28 19:43:08 +0100

    Souk : InsuranceServiceImpl et IInsuranceService pour GB

---
+++ Projet_BoVoyage/src/main/java/fr/adaming/service/InsuranceServiceImpl.java
@@ -0,0 +1,47 @@
+package fr.adaming.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import fr.adaming.dao.IInsuranceDao;
+import fr.adaming.model.Insurance;
+
+@Service
+@Transactional
+public class InsuranceServiceImpl implements IInsruanceService {
+
+	IInsuranceDao insuDao;
+	
+	public void setDao(IInsuranceDao daoToSet) {
+		insuDao = daoToSet;
+		insuDao.setClazz(Insurance.class);
+	}
+	
+	@Override
+	public Insurance getById(int id) {
+		return insuDao.findOne(id);
+	}
+
+	@Override
+	public List<Insurance> getAll() {
+		return insuDao.findAll();
+	}
+
+	@Override
+	public int add(Insurance insurance) {
+		return insuDao.create(insurance);
+	}
+
+	@Override
+	public int update(Insurance insurance) {
+		return insuDao.update(insurance);
+	}
+
+	@Override
+	public int delete(Insurance insurance) {
+		return insuDao.delete(insurance);
+	}
+
+}
