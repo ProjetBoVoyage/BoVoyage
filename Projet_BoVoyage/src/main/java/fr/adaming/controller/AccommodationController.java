640463986435623a6bd05eca1395e46de79eeb6a
IN-BR-002 <IN-BR-002@IN-BR-002>
2019-03-28 22:17:05 +0100

    Ele: Commit Accommodation Controller

--- Projet_BoVoyage/src/main/java/fr/adaming/controller/AccommodationController.java
+++ Projet_BoVoyage/src/main/java/fr/adaming/controller/AccommodationController.java
@@ -1,13 +1,25 @@
 package fr.adaming.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import fr.adaming.model.Destination;
+import fr.adaming.model.Admin;
+import fr.adaming.model.Accommodation;
 import fr.adaming.service.IAccommodationService;
 
 @Controller
@@ -16,24 +28,142 @@ import fr.adaming.service.IAccommodationService;
 public class AccommodationController {
 	
 	// Transformation de l'asso UML en JAVA
-	@SuppressWarnings("unused")
 	@Autowired
 	private IAccommodationService accService;
 	
+	@SuppressWarnings("unused")
+	private Admin admin;
+
+	/** METHODE AJOUTER DESTINATION */
+	// Afficher le Formulaire
 	@RequestMapping(value = "/viewAdd", method = RequestMethod.GET)
 	public String viewAdd(Model modele) {
-		// Lier la destination au modele MVC afin de l'utiliser
-		modele.addAttribute("accAdd", new Destination());
+		// Lier la accination au modele MVC afin de l'utiliser
+		modele.addAttribute("accAdd", new Accommodation());
 		return "addAccommodationPage";
 	}
 
 	// Soumettre le formulauire
-//	@RequestMapping(value = "/submitAdd", method = RequestMethod.POST)
-//	public String submitAdd(@ModelAttribute("accAdd") Accommodation accIn, RedirectAttributes ra) {
-//		// Appel de la méthode service
-//		accService.add(accIn);
-//		return "redirect:viewAcc"; 
-//	}
+	@RequestMapping(value = "/submitAdd", method = RequestMethod.POST)
+	public String submitAdd(@ModelAttribute("accAdd") Accommodation acIn, RedirectAttributes ra, MultipartFile file)
+			throws Exception {
 
+		// Lier la photo récupérée à la accination
+		acIn.setPhoto(file.getBytes());
+
+		// Appel de la méthode service
+		int test = accService.add(acIn);
+
+		if (test != 0) {
+			return "redirect:viewAcc";
+		} else {
+			ra.addFlashAttribute("msg", "Adding Accommodation Failed");
+			return "redirect:viewAdd";
+		}
+
+	}
+
+	/** METHODE MODIFIER UNE DESTINATION */
+	@RequestMapping(value = "/viewUpdate", method = RequestMethod.GET)
+	public String viewUpdate(Model modele) {
+		// Lier la accination au modele MVC afin de l'utiliser
+		modele.addAttribute("accUpdate", new Accommodation());
+		return "updateAccommodationPage";
+	}
+
+	// Soumettre le formulauire
+	@RequestMapping(value = "/submitUpdate", method = RequestMethod.POST)
+	public String submitUpdate(@ModelAttribute("accUpdate") Accommodation acIn, RedirectAttributes ra, MultipartFile file)
+			throws Exception {
+		// Lier la photo récupérée à la accination
+		acIn.setPhoto(file.getBytes());
+
+		// Appel de la méthode service
+		int test = accService.update(acIn);
+		if (test != 0) {
+			return "redirect:viewAcc";
+		} else {
+			ra.addFlashAttribute("msg", "Updating Accommodation Failed");
+			return "redirect:viewUpdate";
+		}
+	}
+
+	@RequestMapping(value = "/updateLink", method = RequestMethod.GET)
+	public String modifLien(Model modele, @RequestParam("pId") int id) {
+		Accommodation acIn = new Accommodation();
+		acIn.setIdAcc(id);;
+		Accommodation acOut = accService.getById(id);
+
+		modele.addAttribute("accUpdate", acOut);
+
+		return "updateAccommodationPage";
+	}
+
+	/** METHODE SUPPRIMER UNE DESTINATION */
+	@RequestMapping(value = "/viewDelete", method = RequestMethod.GET)
+	public String viewDelete(Model modele) {
+		// Lier la accination au modele MVC afin de l'utiliser
+		modele.addAttribute("accDelete", new Accommodation());
+		return "deleteAccommodationPage";
+	}
+
+	// Soumettre le formulauire
+	@RequestMapping(value = "/submitDelete", method = RequestMethod.POST)
+	public String submitDelete(@ModelAttribute("accDelete") Accommodation acIn, RedirectAttributes ra) {
+		// Appel de la méthode service
+		int test = accService.delete(acIn);
+		if (test != 0) {
+			return "redirect:viewAcc";
+		} else {
+			ra.addFlashAttribute("msg", "Deleting Accommodation Failed");
+			return "redirect:viewDelete";
+		}
+	}
+
+	@RequestMapping(value = "/deleteLink", method = RequestMethod.GET)
+	public String supprLien(@RequestParam("pId") int id) {
+		Accommodation acIn = new Accommodation();
+		acIn.setIdAcc(id);
+		accService.delete(acIn);
+
+		return "redirect:viewAcc";
+
+	}
+
+	/** METHODE RECHERCHER UNE DESTINATION */
+	@RequestMapping(value = "/viewSearch", method = RequestMethod.GET)
+	public String viewSearch(Model modele) {
+		// Lier la accination au modele MVC afin de l'utiliser
+		modele.addAttribute("accSearch", new Accommodation());
+		return "searchAccommodationPage";
+	}
+
+	// Soumettre le formulauire
+	@RequestMapping(value = "/submitSearch", method = RequestMethod.POST)
+	public ModelAndView submitSearch(@ModelAttribute("accSearch") Accommodation acIn) {
+
+		return new ModelAndView("searchAccommodationPage", "accommodation", accService.getById(acIn.getIdAcc()));
+
+	}
+
+	/** METHODE AFFICHER TOUTES DESTINATIONS */
+	// Afficher le tableau
+	@RequestMapping(value = "/viewAcc", method = RequestMethod.GET)
+	public ModelAndView viewListDest() {
+		return new ModelAndView("viewAllAccommodationPage", "listAcc", accService.getAll());
+	}
+
+	/** METHODE AFFICHER PHOTO */
+	@RequestMapping(value = "/photoAcc", produces = MediaType.IMAGE_JPEG_VALUE)
+	@ResponseBody
+	public byte[] getPhoto(int idAcc) throws IOException {
+		Accommodation acOut = accService.getById(idAcc);
+		if (acOut.getPhoto() == null) {
+			return new byte[0];
+		} else {
+			return IOUtils.toByteArray(new ByteArrayInputStream(acOut.getPhoto()));
+		}
+	}
 
 }
+
