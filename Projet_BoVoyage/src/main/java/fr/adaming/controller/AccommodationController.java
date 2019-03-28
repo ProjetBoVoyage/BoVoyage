7a6f68c8ba478c73115b1b609732c30bc6fc0289
IN-BR-009 <IN-BR-009@IN-BR-009>
2019-03-28 20:31:51 +0100

    !!!

--- Projet_BoVoyage/src/main/java/fr/adaming/controller/AccommodationController.java
+++ Projet_BoVoyage/src/main/java/fr/adaming/controller/AccommodationController.java
@@ -4,11 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import fr.adaming.model.Accommodation;
 import fr.adaming.model.Destination;
 import fr.adaming.service.IAccommodationService;
+import fr.adaming.service.IAdminService;
+import fr.adaming.service.IDestinationService;
+import fr.adaming.service.IPersonService;
 
 @Controller
 @RequestMapping("/accommodation")
