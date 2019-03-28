a1ae2e64dd09e2561bb6cc467ea98ac169cb7127
IN-BR-011 <IN-BR-011@IN-BR-011>
2019-03-28 21:43:04 +0100

    bobo isurance bad

--- Projet_BoVoyage/src/main/java/fr/adaming/controller/InsuranceController.java
+++ Projet_BoVoyage/src/main/java/fr/adaming/controller/InsuranceController.java
@@ -7,9 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import fr.adaming.model.Admin;
 import fr.adaming.model.Insurance;
 import fr.adaming.service.IInsuranceService;
 
@@ -33,7 +34,108 @@ public class InsuranceController {
 	}
 
 	// submit formulaire
+	@RequestMapping(value = "/submitAdd", method = RequestMethod.POST)
+	public String submitAdd(@ModelAttribute("insuAdd") Insurance iIn, RedirectAttributes ra) {
+		System.out.println(iIn.getName());
+		// appel method service
+		int test=insuService.add(iIn);
 
-	// public String submitAdd(@ModelAttribute("insuAdd") Insurance iIn,
-	// RedirectAttributes ra,)
+		if (test != 0) {
+			return "redirect:viewInsu";
+		} else {
+			ra.addAttribute("msg", "Adding Insurance Failed!");
+			return "redirect:viewAdd";
+		}
+	}
+
+	/** METHODE MODIFIER UNE INSURANCE */
+	@RequestMapping(value = "/viewUpdate", method = RequestMethod.GET)
+	public String viewUpdate(Model modele) {
+		// Lier la destination au modele MVC afin de l'utiliser
+		modele.addAttribute("destUpdate", new Insurance());
+		return "updateInsurancePage";
+	}
+
+	// Soumettre le formulauire
+	@RequestMapping(value = "/submitUpdate", method = RequestMethod.POST)
+	public String submitUpdate(@ModelAttribute("InsuUpdate") Insurance iIn, RedirectAttributes ra) {
+		// Appel de la méthode service
+		int test = insuService.update(iIn);
+		if (test != 0) {
+			return "redirect:viewInsu";
+		} else {
+			ra.addFlashAttribute("msg", "Updating Insurance Failed");
+			return "redirect:viewUpdate";
+		}
+	}
+
+	@RequestMapping(value = "/updateLink", method = RequestMethod.GET)
+	public String modifLien(Model modele, @RequestParam("pId") int id) {
+		Insurance iIn = new Insurance();
+		iIn.setIdInsu(id);
+		Insurance iOut = insuService.getById(id);
+
+		modele.addAttribute("insuUpdate", iOut);
+
+		return "updateInsurancePage";
+	}
+
+	/** METHODE SUPPRIMER UNE DESTINATION */
+	@RequestMapping(value = "/viewDelete", method = RequestMethod.GET)
+	public String viewDelete(Model modele) {
+		// Lier la destination au modele MVC afin de l'utiliser
+		modele.addAttribute("insuDelete", new Insurance());
+		return "deleteInsurancePage";
+	}
+
+	// Soumettre le formulauire
+	@RequestMapping(value = "/submitDelete", method = RequestMethod.POST)
+	public String submitDelete(@ModelAttribute("insuDelete") Insurance iIn, RedirectAttributes ra) {
+		// Appel de la méthode service
+		int test = insuService.delete(iIn);
+		if (test != 0) {
+			return "redirect:viewInsu";
+		} else {
+			ra.addFlashAttribute("msg", "Deleting Insurance Failed");
+			return "redirect:viewDelete";
+		}
+	}
+
+	@RequestMapping(value = "/deleteLink", method = RequestMethod.GET)
+	public String supprLien(@RequestParam("pId") int id) {
+		Insurance iIn = new Insurance();
+		iIn.setIdInsu(id);
+		insuService.delete(iIn);
+
+		return "redirect:listInsu";
+	}
+
+	/** METHODE RECHERCHER UNE DESTINATION */
+	@RequestMapping(value = "/viewSearch", method = RequestMethod.GET)
+	public String viewSearch(Model modele) {
+		// Lier la destination au modele MVC afin de l'utiliser
+		modele.addAttribute("destSearch", new Insurance());
+		return "searchInsurancePage";
+	}
+
+	// Soumettre le formulauire
+	@RequestMapping(value = "/submitSearch", method = RequestMethod.POST)
+	public String submitSearch(@ModelAttribute("insuDelete") Insurance iIn, RedirectAttributes ra) {
+		// Appel de la méthode service
+		Insurance iOut = insuService.getById(iIn.getIdInsu());
+		if (iOut != null) {
+			return "redirect:viewSearch";
+		} else {
+			ra.addFlashAttribute("msg", "Searching Insurance Failed");
+			return "redirect:viewSearch";
+		}
+	}
+
+	/** METHODE AFFICHER TOUTES DESTINATIONS */
+	// Afficher le tableau
+	@RequestMapping(value = "/viewDest", method = RequestMethod.GET)
+	public ModelAndView viewListDest() {
+		return new ModelAndView("viewAllInsurancePage", "listInsu", insuService.getAll());
+	}
+
 }
