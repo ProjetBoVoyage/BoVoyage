b255e8db6c595bfb0721cb74ba28a084440e8471
IN-BR-007 <IN-BR-007@IN-BR-007>
2019-04-03 17:14:07 +0200

    fgvrfbb

--- Projet_BoVoyage/src/main/java/fr/adaming/controller/StartController.java
+++ Projet_BoVoyage/src/main/java/fr/adaming/controller/StartController.java
@@ -3,14 +3,11 @@ package fr.adaming.controller;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
-import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,9 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import fr.adaming.model.Customer;
 import fr.adaming.model.Destination;
-import fr.adaming.service.ICustomerService;
 import fr.adaming.service.IDestinationService;
 
 @Controller
